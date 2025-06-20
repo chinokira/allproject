ec99cff3268461d38b78fd5404584a7aee89c440
chinokira <adrisqf@gmail.com>
2025-06-20 13:00:42 +0200

    First commit

---
+++ java - Copie (2)/notification/src/test/java/fr/formaiton/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.formaiton.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
